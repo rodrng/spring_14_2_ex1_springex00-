53667bff51ac9e5b15e618ce11fea663634dd865
경민 <rodrng@gmail.com>
2022-04-20 11:21:31 +0900

    -

---
+++ src/main/java/com/javagyeongmin/spring_14_2_ex/Studentinformation.java
@@ -0,0 +1,39 @@
+package com.javagyeongmin.spring_14_2_ex;
+
+public class Studentinformation {
+
+	private String name;
+	private String age;
+	private String email;
+	private String address;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/studentView.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	이름 : ${sinfo.name }<br>
+	나이 : ${sinfo.age }<br>
+	이메일 : ${sinfo.email }<br>
+	주소 : ${sinfo.address }<br>
+
+
+</body>
+</html>
